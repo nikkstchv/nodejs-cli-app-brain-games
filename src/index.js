a3ab0dc9977d5ed5e98a8923f269c8a684ee700d
Nikita Kostichev <telerific@gmail.com>
2019-02-09 04:04:05 +0300

    step4

--- src/index.js
+++ src/index.js
@@ -4,22 +4,32 @@ const askName = () => {
 
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-};
 
-const evenNum = () => {
+},
+
+ evenNum = () => {
+
   const userName = askName();
-  for (let i = 0; i < 3; i += 1) {
-    const number = Math.round(1 + Math.random() * (100 - 1));
-    const answer = readlineSync.question(`Question: ${number} `);
+  for (let iter = 0; iter < 3; iter += 1) {
+    const max = 100;
+    const min = 1;
+    const number = Math.round(min + (Math.random() * (max - min))),
+    answer = readlineSync.question(`Question: ${number} `);
     console.log(`Your answer: ${answer}`);
     const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
     if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\r\nLet's try again, ${userName}`);
-    }
-  }
-  return console.log(`Congratulations, ${userName}`);
-};
 
-export { askName, evenNum };
+      console.log('Correct!');
+
+} else {
+
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\r\nLet's try again, ${userName}`);
+
+}
+
+}
+
+return console.log(`Congratulations, ${userName}`);
+
+};
+export {askName, evenNum};
